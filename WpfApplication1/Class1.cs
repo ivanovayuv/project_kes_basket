27e492ac1bdc0d88aa2287c039a76c992a0a4418
Julia Ivanova <zaar.zesaar@gmail.com>
2017-03-06 23:18:04 +0300

    bug fixed

---
+++ WpfApplication1/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class Komanda
+    {
+        public Komanda(string imya, string gorod)
+        {
+            this.imya = imya;
+            this.gorod = gorod;
+        }
+        public string imya;
+        private string Imya
+        {
+            get
+            {
+                return imya;
+            }
+            set
+            {
+                imya = value;
+            }
+        }
+        public string gorod;
+        private string Gorod
+        {
+            get
+            {
+                return gorod;
+            }
+            set
+            {
+                gorod = value;
+            }
+        }
+    }
+}
---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Collections;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Komanda k;
+            List<Komanda> mass = new List<Komanda>();
+            while (true)
+            {
+                string[] str;
+                string stroka = Console.ReadLine();
+                if (stroka == null) break;
+                str = stroka    .Split(',');
+                k = new Komanda(str[0], str[1]);
+                mass.Add(k);
+            }
+        }
+    }
+}
